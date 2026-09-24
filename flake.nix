82675c6d965ead0d45ed400ad9893d3eae887f87
kafu-densha <git@kafu.observer>
2026-09-24 16:04:03 -0700

    actually works this time :sob:

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "A very basic flake";
+  description = "run osu-winello on nixos";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    osu-winello = {
+    osu-winello-source = {
       url = "github:NelloKudo/osu-winello";
       flake = false;
     };
@@ -13,27 +13,60 @@
     {
       self,
       nixpkgs,
-      osu-winello,
+      osu-winello-source,
     }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          wget
-          zenity
-          xdg-desktop-portal
-          unzip
-          steam-run
+      lib = nixpkgs.lib;
+
+      osu-winello-unwrapped = pkgs.stdenv.mkDerivation {
+        pname = "osu-winello";
+        version = "latest";
+        src = osu-winello-source;
+
+        patches = [
+          # use steam-run in the .desktop file for osu
+          (pkgs.fetchpatch2 {
+            url = "https://github.com/kafu-densha/osu-winello/commit/046a5470e8a5214a13f5b3bb6b6e1fb6e322cb4b.patch";
+            hash = "sha256-10HSAlvP23moBR4sNP2wkeXQ++4qzsf4C/TZ+rQDsk4=";
+          })
         ];
 
-        shellHook = ''
-          steam-run ${osu-winello}/osu-winello.sh
-	  steam-run osu-wine
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+        installPhase = ''
+          mkdir -p $out/bin $out/share/osu-winello
+
+          cp -r * $out/share/osu-winello/
+          chmod +x $out/share/osu-winello/osu-winello.sh
+
+          makeWrapper $out/share/osu-winello/osu-winello.sh $out/bin/osu-winello \
+            --prefix PATH : ${
+              pkgs.lib.makeBinPath (
+                with pkgs;
+                [
+                  git
+                  wget
+                  zenity
+                  xdg-desktop-portal
+                  unzip
+                ]
+              )
+            }
         '';
+      };
+
+      osu-winello = pkgs.writeShellScriptBin "osu-winello" ''
+        exec ${lib.getExe pkgs.steam.run} ${osu-winello-unwrapped}/bin/osu-winello "$@"
+      '';
+    in
+    {
+      packages.${system}.default = osu-winello;
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${osu-winello}/bin/osu-winello";
       };
     };
 }
